414598077cb329eff0743d4b5e14754023d51c1d
Majdie Gideir <majdie40@hotmail.com>
2018-11-06 10:41:51 +0000

    Innitial commit.

---
+++ SMCryptoAPIServer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace SMCryptoAPIServer
+{
+    static class Program
+    {
+        public static void startListening()
+        {
+            SMCrypto.StartListening();
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Thread listenThread = new Thread(new ThreadStart(startListening));
+            listenThread.Start();
+            ///Application.EnableVisualStyles();
+            ///Application.SetCompatibleTextRenderingDefault(false);
+            ///Application.Run(new Server());
+
+        }
+    }
+}
